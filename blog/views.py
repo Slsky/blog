771da128b94da34565dc905f787fa833f237bd0f
Slsky <sv.pero@gmail.com>
2017-12-19 12:19:59 +0200

    Add template for index, topics and entry page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Topic
+
+def index(request):
+    """Домашняя страница приложения Блога"""
+    return render(request, 'blog/index.html')
+
+def topics(request):
+    """Выводит список тем."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'blog/topics.html', context)
+
+def topic(request, topic_id):
+    """Вывод одну тему и все её записи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'blog/topic.html', context)
 
 # Create your views here.
